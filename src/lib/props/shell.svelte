804581228fb18e6b3cf50778249498d74d4586dd
mfdev <dev.pajwalp@gmail.com>
2026-05-14 10:27:59 +0530

    feat: colored shell

--- src/lib/props/shell.svelte
+++ src/lib/props/shell.svelte
@@ -1,8 +1,9 @@
 <script>
   import { T } from "@threlte/core";
+  import { BackSide } from "three";
 </script>
 
 <T.Mesh position={[0, 0, 0]}>
   <T.SphereGeometry args={[400, 20, 20]} />
-  <T.MeshStandardMaterial color="purple" />
+  <T.MeshBasicMaterial color="#050816" side={BackSide} />
 </T.Mesh>
